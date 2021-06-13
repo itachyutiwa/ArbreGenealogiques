984b70baa76a520ef78fc1c8658c98d94d969fa9
camara-m <michelinocamara50@gmail.com>
2021-06-13 18:00:22 +0000

    Ajout deS fonctions de 1 à 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,67 @@
 package com.company;
+//import com.google.gson.*;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static <JSONArray> void main(String[] args) {
+
 	// write your code here
            NoeudPersonne a=new NoeudPersonne(1,"CAMARA","Michel","12/05/1995",'M',null);
-           a.AfficherNoeud();
+           NoeudPersonne b=new NoeudPersonne(2,"KONE","GOMPOU","12/05/1995",'M',a);
+           NoeudPersonne c=new NoeudPersonne(3,"Kama","GOMPOU","12/05/1995",'M',a);
+           NoeudPersonne d=new NoeudPersonne(4,"GOORER","BI","12/05/1995",'M',a);
+           NoeudPersonne f=new NoeudPersonne(5,"Marck","konan","12/05/1995",'M',null);
+           NoeudPersonne g=new NoeudPersonne(5,"estelle","kouassi","12/05/1995",'M',f);
+           NoeudPersonne h=new NoeudPersonne(5,"jean","mamy","12/05/1995",'M',g);
+           NoeudPersonne i=new NoeudPersonne(5,"steph","dal","12/05/1995",'M',g);
+           NoeudPersonne j=new NoeudPersonne(5,"harry","kouassi","12/05/1995",'M',i);
+           NoeudPersonne k=new NoeudPersonne(5,"silvestre","ali","12/05/1995",'M',c);
+
+
+            System.out.println("-----------Nombre de noeud crée------------------------------------------");
+            System.out.println(NoeudPersonne.nbreNoeud+" noeuds et le root donc au total "+(NoeudPersonne.nbreNoeud+1)+" noeuds\n");
+            System.out.println("------------------------------------------------------------------------- \n");
+            System.out.println("---------------Affichage complet de notre arbre généalogique-------------");
+            NoeudPersonne.Abre.forEach((n)->n.AfficherNoeud());
+
+
+        System.out.println("---------------Affichage de la liste des enfants d'une personne-------------");
+            a.enfants.forEach((n)->System.out.println(n.getNomPersonne()));
+
+        System.out.println("---------------Affichage de la liste des freres et soeurs d'une personne-------------");
+            System.out.println(d.getNomPersonne()+" a pour frere et soeur :\n");
+            d.freresSoeurs.forEach((n)->System.out.println(n.getNomPersonne()));
+        System.out.println(f.getNomPersonne()+" a pour frere et soeur :\n");
+            f.freresSoeurs.forEach((n)->System.out.println(n.getNomPersonne()));
+
+        System.out.println("---------------Affichage de la liste des cousin et cousine d'une personne -------------");
+        g.AfiichageCousin();
+
+
+        System.out.println("---------------Affichage de la liste des descendant d'une personne-------------");
+        j.descendantPersonne();
+
+        System.out.println("Descendant du root ");
+        NoeudPersonne.Abre.forEach((n)->{
+            if(n.getIdPersonne()==0){
+                n.descendantPersonne();
+            }
+        });
+
+        System.out.println("---------------Affichage de la liste des ascendant d'une personne-------------");
+        h.ascendantPersonne();
+
+        System.out.println("---------------Affichage de frere ainé d'une personne -------------");
+        d.frereAine();
+
+        j.frereAine();
+
+        //System.out.println("JsonAraay::"+JSON);
+        //String json=new json().toJson(NoeudPersonne.Abre);
+        //String Json= new GsonBuilder
+        //String arraytoJson = ObjectMapper.writeValueAsString(NoeudPersonne.Abre);
+        //JSONArray jsArray = new JSONArray(list);
     }
 }
--- src/com/company/NoeudPersonne.java
+++ src/com/company/NoeudPersonne.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -14,18 +15,25 @@ public class NoeudPersonne {
     String dateNaissance;
     char sexePersonne;
     NoeudPersonne parent;
-    List<NoeudPersonne> enfants;
-    List<NoeudPersonne> freresSoeurs;
-    public NoeudPersonne root;
-    public static int nbreNoeud=0;
+    ArrayList<NoeudPersonne> enfants;
+    ArrayList<NoeudPersonne> freresSoeurs;
+    public static NoeudPersonne root;
+    public static int nbreNoeud = 0;
+    public static ArrayList<NoeudPersonne> ArbreComplet;
+
+    public static ArrayList<NoeudPersonne> Abre = new ArrayList<NoeudPersonne>();
 
     //Contructeur du root
     public NoeudPersonne() {
         this.idPersonne = 0;
         this.nomPersonne = "root";
-        this.prenomsPersonne = null;
+        this.prenomsPersonne = "null";
         this.dateNaissance = "01/01/vers ";
         this.sexePersonne = 'N';
+        this.parent = this;
+        this.enfants= new ArrayList<NoeudPersonne>() ;
+        AddNoeud(this);
+
     }
 
     //Constructeur
@@ -33,33 +41,50 @@ public class NoeudPersonne {
         if (nbreNoeud == 0) {
             //creation du root s'il nexiste pas
             creationRoot();
+            nbreNoeud++;
+
             //et creation de l'objet
             this.idPersonne = idPersonne;
             this.nomPersonne = nomPersonne;
             this.prenomsPersonne = prenomsPersonne;
             this.dateNaissance = dateNaissance;
             this.sexePersonne = sexePersonne;
-            if (this.parent == null) {
+            this.enfants=new ArrayList<NoeudPersonne>();
+            this.freresSoeurs=new ArrayList<NoeudPersonne>();
+            if (parent == null) {
                 this.parent = root;
+                AjoutAListeDenfants(this.parent, this);
+                AjoutFreresSoeurs(this.parent,this);
             } else {
                 this.parent = parent;
+                AjoutAListeDenfants(this.parent,this);
+                AjoutFreresSoeurs(this.parent,this);
             }
-            nbreNoeud++;
+
         } else {
             this.idPersonne = idPersonne;
             this.nomPersonne = nomPersonne;
             this.prenomsPersonne = prenomsPersonne;
             this.dateNaissance = dateNaissance;
             this.sexePersonne = sexePersonne;
-            if (this.parent == null) {
+            this.enfants=new ArrayList<NoeudPersonne>();
+            this.freresSoeurs=new ArrayList<NoeudPersonne>();
+            //System.out.print(parent+"\n");
+            if (parent == null) {
                 this.parent = root;
+                AjoutAListeDenfants(this.parent,this);
+                AjoutFreresSoeurs(this.parent,this);
             } else {
                 this.parent = parent;
+                AjoutAListeDenfants(this.parent,this);
+                AjoutFreresSoeurs(this.parent,this);
             }
             nbreNoeud++;
         }
+        AddNoeud(this);
     }
-//Les gatters et les setters
+
+    //Les getters et les setters
     public int getIdPersonne() {
         return idPersonne;
     }
@@ -108,74 +133,170 @@ public class NoeudPersonne {
         this.parent = parent;
     }
 
-    public List<NoeudPersonne> getEnfants() {
+    public ArrayList<NoeudPersonne> getEnfants() {
         return enfants;
     }
 
-    public void setEnfants(List<NoeudPersonne> enfants) {
+    public void setEnfants(ArrayList<NoeudPersonne> enfants) {
         this.enfants = enfants;
     }
 
-    public List<NoeudPersonne> getFreresSoeurs() {
+    public ArrayList<NoeudPersonne> getFreresSoeurs() {
         return freresSoeurs;
     }
 
-    public void setFreresSoeurs(List<NoeudPersonne> freresSoeurs) {
+    public void setFreresSoeurs(ArrayList<NoeudPersonne> freresSoeurs) {
         this.freresSoeurs = freresSoeurs;
     }
 
 
     //Les methodes
     //Affichage de l'arbre généalogique
-    public void afficherArbreGenealogique(){
+    public void afficherArbreGenealogique() {
 
     }
 
 
     //Affichage la liste des enfants
-    public void afficherListeEnfants(int id){
+    public void afficherListeEnfants(int id) {
 
     }
 
     //Editer la liste des enfants
-    public void editerListeEnfants(int id){
+    public void editerListeEnfants(int id) {
 
     }
 
 
     //Affichage des frères et soeurs d'une personne
 
-    public void afficherFresSoeurs(int id){
+    public void afficherFresSoeurs(int id) {
 
     }
 
 
-
     //Affichage de cousins et cousines d'une personne
-    public void afficherCousinsCousines(int id){
+    public void afficherCousinsCousines(int id) {
 
     }
 
 
     //Edition des ascendants d'une personne
-    public void editerAscendants(int id){
+    public void editerAscendants(int id) {
 
     }
 
     //Edition du nom du frère ainés d'une personne
-    public void editerFrereAine(int id){
+    public void editerFrereAine(int id) {
 
     }
 
-    public void editerLienParente(int id1, int id2){
+    public void editerLienParente(int id1, int id2) {
 
     }
 
-    public void creationRoot(){
-        root=new NoeudPersonne();
-    }
-    public void AfficherNoeud(){
-        System.out.println("id"+this.getIdPersonne()+"\n nom :"+this.getNomPersonne()+" \n prenom :"+this.getPrenomsPersonne()+"\n sexe :"+this.getSexePersonne()+"\n date naissance: "+this.getDateNaissance()+"\n parent :"+this.getParent().getNomPersonne());
+    public void creationRoot() {
+        root = new NoeudPersonne();
     }
 
+    public void AfficherNoeud() {
+        System.out.println("id :" + this.getIdPersonne() + "\n nom :" + this.getNomPersonne() + " \n prenom :" + this.getPrenomsPersonne() + "\n sexe :" + this.getSexePersonne() + "\n date naissance: " + this.getDateNaissance() + "\n parent :" + getParent().getNomPersonne());
+    }
+
+    public void AddNoeud(NoeudPersonne n) {
+        Abre.add(n);
+    }
+
+    public void AjoutAListeDenfants(NoeudPersonne pere, NoeudPersonne enf) {
+        Abre.forEach((n)->{
+            if(n==pere){
+               System.out.println(enf.getNomPersonne()+" "+n.getNomPersonne());
+               n.enfants.add(enf);
+            }
+        });
+       //parent.enfants.add(enf);
+    }
+
+    public void AjoutFreresSoeurs(NoeudPersonne pere, NoeudPersonne enf) {
+        Abre.forEach((n)->{
+            if(n==pere){
+               // System.out.println(enf.getNomPersonne()+" "+n.getNomPersonne());
+                n.enfants.forEach((m)->{
+                    if(m!=enf){
+                        m.freresSoeurs.add(enf);
+                        enf.freresSoeurs.add(m);
+                    }
+
+                });
+            }
+        });
+    }
+    public void AfiichageCousin(){
+        if(this.parent.getIdPersonne()==0){
+            System.out.println("Pas de cousin");
+        }else if(this.getIdPersonne()==0){
+            System.out.println("pas de cousin");
+        }else{
+            NoeudPersonne grandPere=this.parent.parent;
+            NoeudPersonne papa =this.parent;
+            Abre.forEach((n)->{
+                if(n==grandPere){
+                    // System.out.println(enf.getNomPersonne()+" "+n.getNomPersonne());
+                    n.enfants.forEach((m)->{
+                        if(m!=papa){
+                            m.enfants.forEach((p)->System.out.println(p.getNomPersonne()));
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+    public void descendantPersonne(){
+        NoeudPersonne personne=this;
+        if(personne.enfants.isEmpty()){
+            System.out.println("pas de descendant");
+        }else{
+            personne.enfants.forEach((n)->
+                    {
+                        System.out.println(n.getNomPersonne());
+                        if(!(n.enfants.isEmpty())){
+                            n.descendantPersonne();
+                        }
+                   }
+            );
+
+        }
+    }
+
+    public void ascendantPersonne(){
+        NoeudPersonne personne=this;
+        if(personne.parent.getIdPersonne()==0){
+            System.out.println("root ");
+        }else{
+            System.out.println(personne.parent.getNomPersonne());
+            personne.parent.AfiichageCousin();
+            personne.parent.freresSoeurs.forEach((n)->
+                    {
+                        System.out.println(n.getNomPersonne());
+                    }
+                     );
+            personne.parent.ascendantPersonne();
+
+
+        }
+    }
+    public void frereAine(){
+
+        NoeudPersonne personne=this;
+        Abre.forEach((n)->{
+            if(n==personne) {
+                if (!(n.freresSoeurs).isEmpty()) {
+                    System.out.println(n.freresSoeurs.get(0).getNomPersonne());
+                }else{
+                    System.out.println("pas de freres ni de soeur");
+                }
+            }
+        });
+    }
 }
